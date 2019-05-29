bcef2d2429611c5b075929ed4f344ee80f1d6545
Paul Rosero <paul_rosero@yahoo.com>
2019-05-29 00:48:50 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,4 +1,17 @@
 class Course
   
   attr_accessor :title, :schedule, :description
+  @@all = []
+
+  def initialize 
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.reser_all
+    
+  end
 end
